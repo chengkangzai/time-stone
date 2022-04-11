a7f52a8d2fab2643c028ce0d6c94f781fd6cbd3d
chengkangzai <pycck@hotmail.com>
2022-04-11 18:39:10 +0800

    finish 2nd half of the stripe

---
+++ app/Jobs/StripeWebhooks/ChargeSucceededJob.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs\StripeWebhooks;
+
+use App\Models\Payment;
+use App\Models\User;
+use App\Notifications\ThanksForDonationNotification;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Spatie\WebhookClient\Models\WebhookCall;
+
+class ChargeSucceededJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private WebhookCall $webhookCall;
+
+    public function __construct(WebhookCall $webhookCall)
+    {
+        $this->webhookCall = $webhookCall;
+    }
+
+    public function handle()
+    {
+        $charge = $this->webhookCall->payload['data']['object'];
+        $user = User::where('stripe_id', $charge['customer'])->first();
+
+        if ($user) {
+            $payment = Payment::where('user_id', $user->id)->whereNull('paid_at')->latest()->first();
+            if ($payment) {
+                $payment->update(['paid_at' => now()]);
+                $user->notify(new ThanksForDonationNotification($charge['receipt_url']));
+            }
+        }
+    }
+
+
+    private function getCharge(): array
+    {
+        return $this->webhookCall->payload['data']['object'];
+    }
+
+}
