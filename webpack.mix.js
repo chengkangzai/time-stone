761ae98a9023c6599d8a6647bc77c5f0f6e5a395
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-04-15 19:02:57 +0800

    introduce tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
+
+mix.postCss("resources/css/app.css", "public/css/tailwind.css", [
+        require("tailwindcss"),
+    ]);
